9567459a66f4cfaab40f35e95a2550992dd8a2b8
Rouven Bissinger <contact@rouvenbissinger.com>
2023-10-18 20:15:12 +0200

    commit before adding v2

--- main.py
+++ main.py
@@ -150,42 +150,12 @@ def split_wd(bids):
 #bids2 = load_bids("./bids/bids02.json") # 10 bids
 #bids3 = load_bids("./bids/bids03.json") # 100 bids
 #bids4 = load_bids("./bids/bids04.json") # 1000 bids
-bids5 = load_bids("./bids/bids05.json") # 1000 bids
+#bids5 = load_bids("./bids/bids05.json") # 1000 bids
 #bids6 = load_bids("./bids/bids06.json") # 10000 bids
 
 
 
-b1 = bids5[0:200]
-b2 = bids5[200:400]
-b3 = bids5[400:600]
-b4 = bids5[600:800]
-b5 = bids5[800:]
+
 
 #print(winner_determination(bids5) == winner_determination_old(bids5))
-t = timer("splited wdp")
-w1 = winner_determination(b1)
-w2 = winner_determination(b2)
-w3 = winner_determination(b3)
-w4 = winner_determination(b4)
-w5 = winner_determination(b5)
-w = w1+w2+w3+w4+w5
-t.time()
-w = winner_determination(w)
-t.time()
-tp = timer("prune+WD")
-b_neu = prune_bids(bids5.copy(), w)
-tp.time()
-w_neu = winner_determination(b_neu+w)
-tp.stop()
-t.stop()
-t2 = timer("full wdp")
-w0 = winner_determination(bids5)
-t2.stop()
-
-#print(l)
-print()
-w_split = split_wd(bids5)
-print(w == w0)
-print(bids_sum(w), bids_sum(w0), bids_sum(w_neu), bids_sum(w_split))
-print(len(w), len(w0), len(w_neu), len(w_split))
 
