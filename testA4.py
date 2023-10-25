c814c49535fd1a34242651d91062d1cd7e22160b
Rouven Bissinger <contact@rouvenbissinger.com>
2023-10-25 16:53:13 +0200

    add test for algo 4

---
+++ testA4.py
@@ -0,0 +1,17 @@
+from wdp import * 
+
+bid_sums = list()
+winners = list()
+times = list()
+
+for i in range(6):
+    bids = load_bids(f"./bids/bids{(i+1):02}.json")
+    print(f"Loaded bids{(i+1):02}.json")
+    t = timer(f"A3: bids{(i+1):02}")
+    winners.append(split_wdp_ed_value(bids))
+    times.append(t.stop())
+    bid_sums.append(bids_sum(winners[i]))
+    print(bid_sums[i])
+
+print(bid_sums)
+print(times)
