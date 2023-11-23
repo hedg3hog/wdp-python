8a5f042e12a5ae3e70f697833787ec169cc37574
Rouven Bissinger <contact@rouvenbissinger.com>
2023-11-23 14:23:44 +0100

    new bids to test

--- bidGen.py
+++ bidGen.py
@@ -1,5 +1,6 @@
 import random
 import json
+import pandas as pd
 
 def gen_bids(n, items, max_items_per_bid=None):
     "returns a list of n bid lists ((item_choice), value)"
@@ -33,5 +34,19 @@ if __name__ == "__main__":
     items1 = json.loads(f.read())
     print(len(items1))
     f.close
+    
+    l = list()
+    x = 20
+    for anz_i in range(10,21):
+        for max_i in range(2,min(16, anz_i), 2):
+            l.append((x,70,anz_i,max_i))
+            x+=1
 
-    save_bids(40000, items1, 50, "./bids/bids18.json")
+    
+    for i in l:
+       save_bids(i[1], range(i[2]), i[3], f"./bids2/comp-bids{i[0]}.json")
+
+    df = pd.DataFrame(l)
+    df.to_csv("bids2/bids2.csv")
+
+    
